8629250bf3f3078a915a2d65e594d6710e63fe10
Omar Alwahiby <omaralwahiby@Omars-MacBook-Pro.local>
2022-11-10 16:05:19 +0300

    edit first page

--- lib/views/screens/first_page.dart
+++ lib/views/screens/first_page.dart
@@ -12,62 +12,66 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 8.1.h,
-            ),
-            Container(
-                height: 43.h,
-                width: 73.w,
-                child: Image(image: AssetImage('images/first.png'))),
-            SizedBox(
-              height: 3.2.h,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 120),
-              child: Text(
-                'حدد اهدافك المالية',
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+
+          child: Column(
+            children: [
+              SizedBox(
+                height: 5.1.h,
               ),
-            ),
-            SizedBox(
-              height: 1.6.h,
-            ),
-            Container(
-              width: 65.8.w,
-              height: 9.h,
-              child: Text(
-                "ماعليك سوى إظافة جميع اهدافك المالية التي توجهك نحو احلامك يمكنك ان ترى الى اي مدى وصلت وماهي المعالم المتبقية لك لتحقق النجاح!",
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+              Container(
+                  height: 37.h,
+                  width: 73.w,
+                  child: Image(image: AssetImage('images/first.png'))),
+              SizedBox(
+                height: 0.2.h,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(140, 55, 0, 0),
-              child: Stack(
-                children: [
-                  Image(image: AssetImage('images/Blob.png')),
-                  Positioned(
-                    left: 220,
-                    top: 180,
-                    child: TextButton(
-                      onPressed: () {
-                        Get.to(SecondPage());
-                      },
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: lightModeScaffoldBgCle,
-                        size: 24.sp,
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Text(
+                  'حدد اهدافك المالية',
+                  style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal),
+                ),
+              ),
+              SizedBox(
+                height: 1.6.h,
+              ),
+              Container(
+                width: 65.8.w,
+                height: 9.h,
+                child: Text(
+                  "ماعليك سوى إظافة جميع اهدافك المالية التي توجهك نحو احلامك يمكنك ان ترى الى اي مدى وصلت وماهي المعالم المتبقية لك لتحقق النجاح!",
+                  style: TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w400),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(61.w, 25.6.h, 0.w, 0.h),
+                child: Stack(
+                  children: [
+                    Image(image: AssetImage('images/Blob.png')),
+                    Positioned(
+                      left: 22.w,
+                      top: 6.h,
+                      child: TextButton(
+                        onPressed: () {
+                          Get.to(SecondPage());
+                        },
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: lightModeScaffoldBgCle,
+                          size: 24.sp,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
