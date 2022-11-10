0b52c62a5c8de55583b8f475e02f9671c2d96ad3
Omar Alwahiby <omaralwahiby@Omars-MacBook-Pro.local>
2022-11-10 11:06:32 +0300

    ss

--- lib/views/screens/first_page.dart
+++ lib/views/screens/first_page.dart
@@ -1,6 +1,7 @@
 import 'package:budget_tracker/views/screens/second_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../constants/colors.dart';
 
@@ -14,11 +15,14 @@ class FirstPage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 67,
+              height: 8.1.h,
             ),
-            Image(image: AssetImage('images/first.png')),
+            Container(
+                height: 43.h,
+                width: 73.w,
+                child: Image(image: AssetImage('images/first.png'))),
             SizedBox(
-              height: 30,
+              height: 3.2.h,
             ),
             Padding(
               padding: const EdgeInsets.only(left: 120),
@@ -31,11 +35,11 @@ class FirstPage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 15,
+              height: 1.6.h,
             ),
             Container(
-              width: 283,
-              height: 84,
+              width: 65.8.w,
+              height: 9.h,
               child: Text(
                 "ماعليك سوى إظافة جميع اهدافك المالية التي توجهك نحو احلامك يمكنك ان ترى الى اي مدى وصلت وماهي المعالم المتبقية لك لتحقق النجاح!",
                 style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
@@ -45,9 +49,7 @@ class FirstPage extends StatelessWidget {
               padding: EdgeInsets.fromLTRB(140, 55, 0, 0),
               child: Stack(
                 children: [
-                  Image(
-                    image: AssetImage('images/Blob.png'),
-                  ),
+                  Image(image: AssetImage('images/Blob.png')),
                   Positioned(
                     left: 220,
                     top: 180,
@@ -58,7 +60,7 @@ class FirstPage extends StatelessWidget {
                       child: Icon(
                         Icons.arrow_forward,
                         color: lightModeScaffoldBgCle,
-                        size: 35,
+                        size: 24.sp,
                       ),
                     ),
                   ),
